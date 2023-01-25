f7447d1986d50a1ef5e55e70e4df5d5b7ab3cc7f
evilraptor <43874288+evilraptor@users.noreply.github.com>
2023-01-25 22:24:13 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "Class_BigInt.h"
+
+int main()
+{
+    //std::string str1 = "4574988923784";
+    //std::string str2 = "7892657887452";
+    //BigInt number1 = BigInt(str1);
+    //BigInt number2 = BigInt(str2);
+
+    //std::cout << number1 + number2;
+
+    BigInt number3 = BigInt(-2);
+    BigInt number4 = BigInt(-4);
+    number3 *= number4;
+    std::cout << number3;
+
+    //std::cout << ((BigInt(1) * BigInt(0))==BigInt(0));
+
+    return 0;
+}
